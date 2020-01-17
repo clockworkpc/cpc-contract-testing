25fc1bc807448b9ede499cfe209e52a33a0037ca
alexander <agarber@profectusgroup.com>
2020-01-18 08:08:57 +1100

    shell scripts

--- spec/contractor/cpc_library_spec.rb
+++ spec/contractor/cpc_library_spec.rb
@@ -74,8 +74,6 @@ RSpec.describe Contractor::CpcLibrary do
     end
 
     it 'should PUT a book by ID' do
-      book_hsh_ary = JSON.parse(File.read(new_books_json_path))
-      last_book_hsh = book_hsh_ary[-2]
       bookID_str = subject.get_latest_book['_id']
       book_hsh = {
         _id: bookID_str,
@@ -93,66 +91,5 @@ RSpec.describe Contractor::CpcLibrary do
 
       puts "Title after PUT: #{subject.get_latest_book['title']}"
     end
-
-    # it 'should PUT a contact by ID' do
-    #   id_str = '5e1fda38e2f0747c7c67a952'
-    #
-    #   put1_contact_hsh = {
-    #     _id: id_str,
-    #     christianName: 'John',
-    #     surname: 'Doe',
-    #     email: 'johndoe@test.com',
-    #     company: 'Foo Bar Pty Ltd',
-    #     phone: 123_456_789
-    #   }
-    #
-    #
-    #   put2_contact_hsh = {
-    #     _id: id_str,
-    #     christianName: 'Hamish',
-    #     surname: 'MacDonald',
-    #     email: 'hamishmacdonald@test.com',
-    #     company: 'Bar Foo Pty Ltd',
-    #     phone: 987_654_321
-    #   }
-    #
-    #   res1 = subject.put_contact(put1_contact_hsh)
-    #   expect(res1.code).to eq(200)
-    #   expect(res1.body['_id'].length).to eq(24)
-    #   expect(res1.body['christianName']).to eq(put1_contact_hsh[:christianName])
-    #   expect(res1.body['surname']).to eq(put1_contact_hsh[:surname])
-    #   expect(res1.body['email']).to eq(put1_contact_hsh[:email])
-    #   expect(res1.body['company']).to eq(put1_contact_hsh[:company])
-    #   expect(res1.body['phone']).to eq(put1_contact_hsh[:phone])
-    #   expect(res1.headers['x-powered-by']).to eq('Express')
-    #
-    #   res1.body.each {|k,v| puts "#{k}: #{v}"}
-    #
-    #   res2 = subject.put_contact(put2_contact_hsh)
-    #   expect(res2.code).to eq(200)
-    #   expect(res2.body['_id'].length).to eq(24)
-    #   expect(res2.body['christianName']).to eq(put2_contact_hsh[:christianName])
-    #   expect(res2.body['surname']).to eq(put2_contact_hsh[:surname])
-    #   expect(res2.body['email']).to eq(put2_contact_hsh[:email])
-    #   expect(res2.body['company']).to eq(put2_contact_hsh[:company])
-    #   expect(res2.body['phone']).to eq(put2_contact_hsh[:phone])
-    #   expect(res2.headers['x-powered-by']).to eq('Express')
-    #
-    #   res2.body.each {|k,v| puts "#{k}: #{v}"}
-    # end
-    #
-    # it 'should DELETE the latest_contact contact' do
-    #   protected_id_ary = ['5e1fd536e2f0747c7c67a949', '5e1fda38e2f0747c7c67a952']
-    #
-    #   all_contacts = subject.get_all_contacts.body
-    #   deletable_contacts = all_contacts.reject { |h| protected_id_ary.include?(h['_id']) }
-    #   deletable_ids = deletable_contacts.map { |h| h['_id'] }
-    #   expect(deletable_ids.count).to eq(all_contacts.count - 2)
-    #   id_to_delete = deletable_ids.last
-    #
-    #   res = subject.delete_latest_contact(protected_id_ary)
-    #   expect(res.code).to eq(200)
-    #   expect(res.body["id"]).to eq(id_to_delete)
-    # end
   end
 end
